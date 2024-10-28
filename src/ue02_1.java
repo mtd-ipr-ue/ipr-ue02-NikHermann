e3b364d1cfa171a6e55dc4f157f21450e9670ccc
Nikolai <s241038026@fhooe.at>
2024-10-28 20:15:10 +0100

    ue02_1_NikolaiHermann

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -1,7 +1,26 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        int d;
+        System.out.print("Gib eine Zahl ein: ");
+        int a = In.readInt();
+        d = a;
+        System.out.print("Gib noch eine Zahl ein: ");
+        int b = In.readInt();
+        if(b > d) {
+            d = b;
+        }
+        System.out.print("Gib noch eine Zahl ein: ");
+        int c = In.readInt();
+        if(c > d) {
+            d = c;
+        }
+
+        Out.println("Die größte Zahl ist: " + d);
+
+        
+
     }
 }
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -1,7 +1,44 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        
+        Out.print("Gib die Jahreszahl ein: ");
+        int j = In.readInt();
+        
+        Out.print("Gib einen Monatswert an (1 für Jan; 12 für Dez): ");
+        int m = In.readInt();
+
+        
+        
+        switch(m) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                System.out.println("Der Monat hat 31 Tage.");
+                break;
+            case 4, 6, 9, 11:
+                System.out.println("Der Monat hat 30 Tage.");
+                break;
+            case 2:
+                if(j % 4 == 0) {
+                    if(j % 100 == 0) {
+                        if(j % 400 == 0) {
+                            System.out.println("Der Monat hat 29 Tage.");
+                        }
+                        else {
+                            System.out.println("Der Monat hat 28 Tage.");
+                        }
+                    }
+                    else {
+                        System.out.println("Der Monat hat 29 Tage.");
+                    }
+                }
+                else {
+                    System.out.println("Der Monat hat 28 Tage.");
+                }
+                break;
+        }
+
+        }
     }
-}
+
