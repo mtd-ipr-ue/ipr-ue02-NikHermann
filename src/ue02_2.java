856b02330df64de42224d8e759a453b694e6b4e9
Nikolai <s241038026@fhooe.at>
2024-10-28 20:15:25 +0100

    ue02_2_NikolaiHermann

--- src/ue02_2.java
+++ src/ue02_2.java
@@ -4,40 +4,52 @@ import libraries.Out;
 public class ue02_2 {
     public static void main(String[] args) {
         
+        
         Out.print("Gib die Jahreszahl ein: ");
         int j = In.readInt();
-        
-        Out.print("Gib einen Monatswert an (1 für Jan; 12 für Dez): ");
-        int m = In.readInt();
 
-        
-        
-        switch(m) {
-            case 1, 3, 5, 7, 8, 10, 12:
-                System.out.println("Der Monat hat 31 Tage.");
-                break;
-            case 4, 6, 9, 11:
-                System.out.println("Der Monat hat 30 Tage.");
-                break;
-            case 2:
-                if(j % 4 == 0) {
-                    if(j % 100 == 0) {
-                        if(j % 400 == 0) {
-                            System.out.println("Der Monat hat 29 Tage.");
+        int m; //monate
+        int x = 0;
+
+        while(x == 0) {
+
+            Out.print("Gib einen Monatswert an (1 für Jan; 12 für Dez): ");
+            m = In.readInt();
+
+            if(m < 1 || m > 12) {
+                System.out.println("Gib einen Monatswert zwischen 1 und 12 aus!");
+            }
+            else {
+                switch(m) {
+                    case 1, 3, 5, 7, 8, 10, 12: //Monate mit 31 Tagen
+                        System.out.println("Der Monat hat 31 Tage.");
+                        break;
+                    case 4, 6, 9, 11: //Monate mit 30 Tagen
+                        System.out.println("Der Monat hat 30 Tage.");
+                        break;
+                    case 2: //Februar
+                        if(j % 4 == 0) {
+                            if(j % 100 == 0) {
+                                if(j % 400 == 0) {
+                                    System.out.println("Der Monat hat 29 Tage.");
+                                }
+                                else {
+                                    System.out.println("Der Monat hat 28 Tage.");
+                                }
+                            }
+                            else {
+                                System.out.println("Der Monat hat 29 Tage.");
+                            }
                         }
                         else {
                             System.out.println("Der Monat hat 28 Tage.");
                         }
-                    }
-                    else {
-                        System.out.println("Der Monat hat 29 Tage.");
-                    }
+                        break;
                 }
-                else {
-                    System.out.println("Der Monat hat 28 Tage.");
-                }
-                break;
+            x++;
+            }
         }
+       
 
         }
     }
--- src/ue02_3.java
+++ src/ue02_3.java
@@ -1,7 +1,25 @@
+import libraries.In;
 import libraries.Out;
 
 public class ue02_3 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+
+        Out.print("Gib die x-Koordinate ein: ");
+        double x = In.readDouble();
+        Out.print("Gib die y-Koordinate ein: ");
+        double y = In.readDouble();
+        
+        double d = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));  //Abstand des Punktes vom Ursprung
+
+        boolean inSquare = ((x >= 0 && x <= 4) && (y >= 0 && y <= 4));  //liegt Punkt innerhalb von Rechteck?
+
+        boolean outCircle = (d > 4);    //liegt Punkt außerhalb von Kreis
+
+        if(inSquare && outCircle) {         //wenn beide Bedingungen true sind
+            System.out.println("Punkt liegt auf grüner Fläche!");
+        }
+        else {
+            System.out.println("Punkt liegt nicht auf grüner Fläche!");
+        }
     }
 }
